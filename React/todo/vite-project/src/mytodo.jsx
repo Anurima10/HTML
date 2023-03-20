da0d7f49dc8f01542b47d36006be5afbb25fa59b
anurima10 <anurima.yadav01@gmail.com>
2023-03-21 01:34:24 +0530

    todo

---
+++ React/todo/vite-project/src/mytodo.jsx
@@ -0,0 +1,61 @@
+import { Component } from "react";
+
+class FormElement extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          firstName: "",
+          lastName: "",
+          email: "",
+        };
+      }
+    
+      handleInputChange = (e) => {
+        console.log(e);
+      };
+    
+      render() {
+        const { firstName, lastName, email } = this.state;
+        console.log("Re-render");
+        return (
+          <>
+            <div className="form-element">
+              <label htmlFor="firstName">First Name: {firstName}</label>
+              <Input
+                type="text"
+                name="firstName"
+                placeholder="Enter first name"
+                onChange={(e) => {
+                  const { value, name } = e.target;
+                  console.log("e: ", value, name);
+                }}
+              />
+            </div>
+    
+            <div className="form-element">
+              <label htmlFor="lastName">Last Name: {lastName}</label>
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Enter last name"
+                onChange={this.handleInputChange}
+              />
+            </div>
+    
+            <div className="form-element">
+              <label htmlFor="email">Email: {email}</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter email address"
+                onChange={this.handleInputChange}
+              />
+            </div>
+          </>
+        );
+      }
+    }
+    
+    export default FormElement;
+    
